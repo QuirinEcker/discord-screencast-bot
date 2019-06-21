62847e053eba28f28048dd6e1fdc57060775c169
Quirin Ecker <quirin.ecker@bajupa.com>
2019-06-21 18:20:58 +0200

    updated gitignore

---
+++ DiscordBot/index.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const auth = require('./auth.json');
+const opn = require('opn');
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+client.on('message', (msg) => {
+    if (msg.content === '§screencast') {
+        if (msg.channel.id == 410537317497831424) {
+            if (msg.member.voiceChannelID != null) {
+                msg.channel.send(`http://discordapp.com/channels/388433193638035457/${msg.member.voiceChannelID}`);
+            } else {
+                msg.channel.send("```diff\n-Please join a channel\n```");
+            }
+        } else {
+            msg.channel.send("```diff\n-Please send bot commands into the bot chanel\n```");
+            let commandMessage = msg.channel.lastMessageID;
+            setTimeout(() => {
+                console.log(msg.channel.messages.get(msg.channel.lastMessageID).delete());
+                console.log(msg.channel.messages.get(commandMessage).delete());
+            }, 5000);
+        }
+    }
+});
+
+console.log(client.login(auth.token));
