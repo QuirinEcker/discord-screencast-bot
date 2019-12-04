23e1229cebd179a01c1cca9754ea8dbb2bd3587c
Quirin <quirin.ecker@bajupa.com>
2019-12-04 11:18:42 +0100

    removed debug messages

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const token = require('./token').token;
 const whitelist = Array.from(require("./whitelist.json"))
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
     whitelist.forEach(item => console.log(item));
 });
 
@@ -18,8 +17,6 @@ client.on('message', (msg) => {
                 msg.channel.send("```diff\n-Please join a channel\n```");
             }
 
-            console.log(whitelist.indexOf(msg.guild.id))
-            console.log(msg.guild.id);
             if (!(whitelist.indexOf(msg.guild.id) > -1)) msg.channel.send(`For Bugs visit the **Implodium**  discord server https://discord.gg/N8Qk6gA`)
             else msg.channel.send(`**For Bugs send messages into** ${msg.guild.channels.get("651726057929441281")} **or contact** ${msg.guild.members.get("204674824910405633")}`);
         }
