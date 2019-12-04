84245492388eea7627bda3228a41f647fa18baeb
Quirin <quirin.ecker@bajupa.com>
2019-12-04 11:15:56 +0100

    if on a whitelisted server a message will be send to contact LebenderFux or send message into Bot Channel

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ client.on('message', (msg) => {
 
             console.log(whitelist.indexOf(msg.guild.id))
             console.log(msg.guild.id);
-            if (!(whitelist.indexOf(msg.guild.id) > -1)) msg.channel.send(`For Bugs visit the **Implodium**  discord server https://discord.gg/N8Qk6gA`);
+            if (!(whitelist.indexOf(msg.guild.id) > -1)) msg.channel.send(`For Bugs visit the **Implodium**  discord server https://discord.gg/N8Qk6gA`)
+            else msg.channel.send(`**For Bugs send messages into** ${msg.guild.channels.get("651726057929441281")} **or contact** ${msg.guild.members.get("204674824910405633")}`);
         }
 });
 
